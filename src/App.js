463c8a230bd38bb0337292494ecc2bdd85eff3cd
daadesina <daadesina1@gmail.com>
2024-09-27 01:09:58 +0000

    app creation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './src_bootstrap.css'
+import './my_home.css';
+import 'https://kit.fontawesome.com/c5e4d5e6fa.js';
+import Home from './my_home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
